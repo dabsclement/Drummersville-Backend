a93f8bc469025bd313aeb25d6e798f94bb283441
Oluwaseun Dabiri <dadsclement@gmail.com>
2020-08-15 12:58:42 +0100

    router defined

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const cors = require('cors');
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
 const mongoose = require("mongoose");
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const podcast = require('./routes/podcasts');
+
 
 var app = express();
 
@@ -26,6 +28,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/podcast", podcast);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
